b26ed2712e8a884e33d11c15dda227090a78636c
Diego Gonzales <dgonzalesruiz@gmail.com>
2022-11-28 13:48:00 -0500

    Update detalles_curso.php

--- detalles_curso.php
+++ detalles_curso.php
@@ -42,7 +42,7 @@ $e = getCursoId($_GET["id"]);
 					 </svg>
 					 <div id="msj-pago" hidden>
 						 <div class="alert alert-success d-flex align-items-center" role="alert">
-						  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+						  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="compra exitosa"><use xlink:href="#check-circle-fill"/></svg>
 						  ¡Pago completado!
 						 </div>
 				     </div>
