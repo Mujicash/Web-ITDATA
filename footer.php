1dccdb3d949880fcd8d77b0b5ec5dc9aecbe3991
Diego Gonzales <dgonzalesruiz@gmail.com>
2022-11-28 19:21:12 -0500

    jquery

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
   <div class="container"><p class="m-0 small">Copyright &copy; Grupo de Investigación ITDATA 2022</p></div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
 <script src="assets/js/scripts.js"></script>
 </body>
 </html>
