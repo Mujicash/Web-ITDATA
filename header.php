2f6732862d29c302e4bee3b9ee55ff4ffa48b158
Diego Gonzales <dgonzalesruiz@gmail.com>
2022-11-29 20:58:38 -0500

    iconos

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
   <link href="assets/css/styles.css" rel="stylesheet"/>
   <link href="assets/css/styles2.css" rel="stylesheet"/>
   <link href="assets/css/custom.css" rel="stylesheet"/>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
   <!-- PayPal JavaScript SDK -->
   <script src="https://www.paypal.com/sdk/js?client-id=test&currency=USD"></script>
   <!-- --------------------- -->
